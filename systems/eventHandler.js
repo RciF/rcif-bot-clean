0cfe19e4482fd9ee0a44a77ccf8b677475ddb119
RciF <rcif@email.com>
2026-04-11 01:24:33 +0300

    feat: support nested event folders in eventHandler

--- systems/eventHandler.js
+++ systems/eventHandler.js
@@ -2,80 +2,78 @@ const fs = require("fs")
 const path = require("path")
 const logger = require("./loggerSystem")
 
-module.exports = (client) => {
+function getAllEventFiles(dir) {
+  let results = []
 
+  const items = fs.readdirSync(dir)
+
+  for (const item of items) {
+    const fullPath = path.join(dir, item)
+    const stat = fs.statSync(fullPath)
+
+    if (stat.isDirectory()) {
+      results = results.concat(getAllEventFiles(fullPath))
+    } else if (item.endsWith(".js")) {
+      results.push(fullPath)
+    }
+  }
+
+  return results
+}
+
+module.exports = (client) => {
   const eventsPath = path.join(__dirname, "../events")
 
   if (!fs.existsSync(eventsPath)) {
-
     logger.warn("EVENTS_FOLDER_NOT_FOUND")
     return
-
   }
 
   let eventFiles = []
 
   try {
-
-    eventFiles = fs
-      .readdirSync(eventsPath)
-      .filter(file => file.endsWith(".js"))
-
+    eventFiles = getAllEventFiles(eventsPath)
   } catch (err) {
-
     logger.error("EVENT_FOLDER_READ_FAILED", {
       error: err.message
     })
-
     return
   }
 
   let loaded = 0
   let skipped = 0
 
-  for (const file of eventFiles) {
-
+  for (const filePath of eventFiles) {
     try {
-
-      const filePath = path.join(eventsPath, file)
-
       delete require.cache[require.resolve(filePath)]
 
       const event = require(filePath)
 
       if (!event || !event.name || typeof event.execute !== "function") {
-
-        logger.warn(`INVALID_EVENT_FILE ${file}`)
+        const fileName = path.relative(eventsPath, filePath)
+        logger.warn(`INVALID_EVENT_FILE ${fileName}`)
         skipped++
         continue
-
       }
 
       if (event.once) {
-
         client.once(event.name, (...args) => event.execute(...args, client))
-
       } else {
-
         client.on(event.name, (...args) => event.execute(...args, client))
-
       }
 
       loaded++
 
-      logger.info(`EVENT_LOADED ${event.name}`)
-
+      const fileName = path.relative(eventsPath, filePath)
+      logger.info(`EVENT_LOADED ${event.name} (${fileName})`)
     } catch (error) {
-
+      const fileName = path.relative(eventsPath, filePath)
       logger.error("EVENT_LOAD_FAILED", {
-        file,
+        file: fileName,
         error: error.message
       })
-
       skipped++
-
     }
-
   }
 
   logger.success(`EVENTS_LOADED ${loaded}`)
@@ -83,5 +81,4 @@ module.exports = (client) => {
   if (skipped > 0) {
     logger.warn(`EVENTS_SKIPPED ${skipped}`)
   }
-
 }
